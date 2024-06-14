ca17ffa01f745ef4f5b321a618e3f26ca4808aa7
JunSeoKim <seo020815@naver.com>
2024-06-14 18:22:43 +0900

    탭 컨텐츠 수정

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 let tabmenu = $(".tabmenu li");
-let tab = $(".tab > div");
-
-
-tabmenu.click(function(e){
-  e.preventDefault();
-  tabmenu.removeClass('active');
-  $(this).addClass('active');
-  let target = $(this).find('a').attr('href');
-  tab.removeClass('active');
-  $(target).addClass('active');
-});
-
-let tabMenu = $(".tabmenu li");
-let tab_content = $(".tab > div");
+let tab = $(".tab_wrapper > div");
 
 
 tabmenu.click(function(e){
