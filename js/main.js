e07dd6d34621d434ac4c21fe39e53bed9e81c64a
JunSeoKim <seo020815@naver.com>
2024-06-14 19:33:53 +0900

    메인메뉴 잘못건들여서 큰일남

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+
+/*탭 컨텐츠*/
 let tabmenu = $(".tabmenu li");
 let tab = $(".tab_wrapper > div");
-
 
 tabmenu.click(function(e){
   e.preventDefault();
@@ -10,3 +11,14 @@ tabmenu.click(function(e){
   tab.removeClass('active');
   $(target).addClass('active');
 });
+
+/*메인메뉴 슬라이드 다운*/
+/*
+let mainMenu = $('.main-menu');
+let main_ul = $('.main-menu ul ul');
+mainMenu.mouseover(function(){
+  main_ul.stop().slideDown();
+}).mouseout(function(){
+  main_ul.stop().slideUp();
+});
+*/
