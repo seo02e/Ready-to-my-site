8383f441f780ddf14da09f22cea14bcaa9c97f62
JunSeoKim <seo020815@naver.com>
2024-06-16 15:18:29 +0900

    슬라이드 수정

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ tabmenu.click(function(e){
   $(target).addClass('active');
 });
 
-/*메인메뉴 슬라이드 다운*/
+/*메인메뉴의 서브메뉴 다운*/
 
 let mainMenu = $('.main-menu');
 let main_ul = $('.main-menu ul ul');
@@ -23,17 +23,46 @@ mainMenu.mouseover(function(){
 });
 
 /*슬라이드*/
-let slideContainer = $('swapper');
-let slide = slideContainer.find('.slide');
+
+let swapper = $('.swapper');
+let slideContainer = swapper.find('.slide-img'); // ul 요소 선택
+let slide = slideContainer.find('li');
 let slideCount = slide.length;
 let currentIdx = 0;
+
+function goToSlide(idx) {
+  slideContainer.animate({
+    marginTop: -400 * idx + 'px'
+  });
+  currentIdx = idx;
+}
+
+$('.radio-buttons input[type="radio"]').click(function() {
+  let idx = $(this).index();
+  goToSlide(idx / 2); // 라벨이 두 번째 요소이기 때문에 인덱스를 2로 나눔
+});
+
+function autoSlide() {
+  setInterval(function() {
+    let nextIdx = (currentIdx + 1) % slideCount;
+    goToSlide(nextIdx);
+    $(`.radio-buttons input[type="radio"]:eq(${nextIdx})`).prop('checked', true);
+  }, 3000);
+}
+
+autoSlide();
+/*
+let swapper = $('.swapper');
+let slide = swapper.find('li');
+let slideCount = slide.length;
+let currentIdx =0;
 function autoSlide(){
   setInterval(function(){
-    let nextIdx = (currentIdx + 1) % slideCouont;
-    slideContainer.animate({
-      marginTop: -400 *nextIdx +'px'
-    });
+    let nextIdx = (currentIdx + 1) % slideCount;
+    slide.removeClass('active');
+    slide.eq(nextIdx).addClass('active');
     currentIdx = nextIdx;
   }, 3000);
 }
 autoSlide();
+*/
